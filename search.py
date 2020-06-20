4eed9197042ee1e24c9bd05107c68c46e7b775e0
urasha <43294860+urasha@users.noreply.github.com>
2020-06-20 16:29:51 +1000

    Add files via upload

--- search.py
+++ search.py
@@ -1,1 +1,16 @@
 import requests
+import lxml.html
+
+
+def search_text(word: str, url):
+    try:
+        url += word
+        api = requests.get(url)
+
+        tree = lxml.html.document_fromstring(api.text)
+        text = tree.xpath('//*[@id="mw-content-text"]/div/p[1]')[0].text_content()
+
+        return text, url
+
+    except Exception:
+        return "Sorry, I don't know this word &#128532"
